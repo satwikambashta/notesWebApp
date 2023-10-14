e2175361e34e22fc06b4f4853cd8b6e6d121fa04
satwikambashta <simmysatwik03@gmail.com>
2023-10-15 00:04:32 +0530

    sec

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,21 @@
+import "dotenv/config";
+import mongoose from "mongoose";
 import express from "express"
 
-const app=express();
-const port = 5000;
+const app = express();
+const port = process.env.PORT;
 
 //http endpoint get request
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hello");
 })
 
-app.listen(port!, () => {
-    console.log("Server running on port" + port);
-})
+mongoose.connect(process.env.MONGO_CONNECTION_STRING!)
+    .then(() => {
+        console.log("Connected to mongoDB through mongoose");
+        app.listen(port!, () => {
+            console.log("Server running on port" + port);
+        })
+    })
+    .catch(console.error);
+
