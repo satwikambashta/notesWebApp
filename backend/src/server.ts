614fce92059a77aadd67147c6f1c31ef9e298f71
satwikambashta <simmysatwik03@gmail.com>
2023-10-15 09:50:31 +0530

    sec

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,18 @@
 import "dotenv/config";
 import mongoose from "mongoose";
-import express from "express"
+import express from "express";
+import env from"./util/validateEnv";
 
 const app = express();
-const port = process.env.PORT;
+const port = env.PORT;
 
+//we made validateEnv to set value of port and connection strings to not be undefined. server will give error if type is not string or number here
 //http endpoint get request
 app.get("/", (req, res) => {
     res.send("Hello");
 })
 
-mongoose.connect(process.env.MONGO_CONNECTION_STRING!)
+mongoose.connect(env.MONGO_CONNECTION_STRING)
     .then(() => {
         console.log("Connected to mongoDB through mongoose");
         app.listen(port!, () => {
