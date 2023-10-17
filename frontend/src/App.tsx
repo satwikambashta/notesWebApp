2ee303267698a7e9f65213c87bac8c4d60197e93
satwikambashta <simmysatwik03@gmail.com>
2023-10-17 19:07:21 +0530

    frontend starts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,28 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import { Button } from 'react-bootstrap';
+import {Note} from "./models/note";
 
 function App() {
-  const [clickCount, setClickCount] = useState(0);
+  const [notes, setNotes] = useState<Note[]>([]);
+  useEffect(()=> {
+    
+    async function loadNotes(){
+      try {
+        const response= await fetch("/api/notes", {method: "GET"});
+        const notes = await response.json();
+        setNotes(notes)
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
+      
+    }
+    loadNotes();
+  }, [])  //empty array to execute once
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Button onClick={()=>setClickCount(clickCount+1)}>
-            Count: {clickCount}
-          </Button>
-        </p>
-      </header>
+      {JSON.stringify(notes)}
     </div>
   );
 }
